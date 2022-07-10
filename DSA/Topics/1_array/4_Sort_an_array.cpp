44913162d9e988d9cc191836ab8bf6869feccf2b
Vineet <vineet@vrikster.com>
2022-07-11 00:52:03 +0530

    one more question answered

--- DSA/Topics/1_array/4_Sort_an_array.cpp
+++ DSA/Topics/1_array/4_Sort_an_array.cpp
@@ -1,3 +1,27 @@
 /*
-    Q- https://practice.geeksforgeeks.org/problems/sort-an-array-of-0s-1s-and-2s4231/1
+    Q- https://leetcode.com/problems/sort-an-array/
 */
+
+class Solution {
+public:
+	void countingsort(vector<int>& nums){
+		int n=nums.size();
+		vector<int> count(100001,0);
+		for(auto& x:nums){
+			count[x+50000]++;
+		}
+		for(int i=1;i<count.size();i++){
+			count[i]+=count[i-1];
+		}
+		vector<int> ans(n);
+		for(auto& v:nums){
+			ans[count[v+50000]-1]=v;
+			count[v+50000]--;
+		}
+		nums=ans;
+	}
+	vector<int> sortArray(vector<int>& nums) {
+		countingsort(nums);
+		return nums;
+	}
+};
