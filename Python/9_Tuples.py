cd4df5110e92f608e315df800ebd4ed126488c92
Vineet <vineet@vrikster.com>
2022-07-22 22:09:58 +0530

    typles revision

---
+++ Python/9_Tuples.py
@@ -0,0 +1,6 @@
+"""
+A tuple is created by placing all the items(elements) inside parentheses(), separated by commas. The
+parentheses are optional, however, it is a good practice to  use them.
+
+Tuples are immutable. This means that elements of tuple cannot be changed once it has been assigned.
+"""
