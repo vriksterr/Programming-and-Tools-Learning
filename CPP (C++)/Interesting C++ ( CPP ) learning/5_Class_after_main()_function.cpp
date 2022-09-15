12cbd4c635412717f7e21080746027460d1b956e
vineet <vineet@vrikster.com>
2022-09-15 16:39:57 +0530

    Added more  learnings about class and class functions

--- CPP (C++)/Interesting C++ ( CPP ) learning/5_Class_after_main()_function.cpp
+++ CPP (C++)/Interesting C++ ( CPP ) learning/5_Class_after_main()_function.cpp
@@ -9,7 +9,9 @@ using namespace std;
 //Prototypes
 class ClassOne;
 ClassOne *make_class_one();     //like int func() this is also a function whose return type is an object
-void use_class(ClassOne *x);
+ClassOne function_of_no_use();
+void use_object();
+void use_class(ClassOne* x);
 
 
 //Main Function
@@ -17,6 +19,17 @@ int main()
 {
     ClassOne *one = make_class_one();
     use_class(one);
+    //
+
+    cout<<make_class_one(); //outputs an address of the function as it is a pointer
+    
+    /*The reason it wont work is coz the compiler looks up and cant find the class
+    defination so thats why we use the trick above*/
+ //   ClassOne two = function_of_no_use();
+
+    //
+    use_object();
+    //
     return 0;
 }
 
@@ -24,19 +37,33 @@ int main()
 //Definations
 class ClassOne
 {
-    public:
-        void coolSaying()
-        {
-            cout << "Cool stuff yo!" << endl;
-        }
+public:
+    void coolSaying()
+    {
+        cout << "Cool stuff yo!" << endl;
+    }
 };
 
 ClassOne *make_class_one()
 {
-     return new ClassOne;   // Bad idea, use uniqe_ptr, but I'm lazy.
+    return new ClassOne;   // Bad idea, use uniqe_ptr, but I'm lazy.
 }
 
-void use_class(ClassOne *x)
+ClassOne function_of_no_use() {
+    ClassOne func;
+    return func;
+}
+
+
+void use_class(ClassOne* x)
 {
     x->coolSaying();
+}
+
+
+//extra
+void use_object() {
+    ClassOne func;
+    cout << endl;
+    func.coolSaying();
 }
