e0871099f2079d9da0d48ac68d5eb15153b7e37f
vineet <vineet@vrikster.com>
2022-09-19 12:10:14 +0530

    DSA Questions solved

--- Data Structures and Algorithms with C++/Questions (Topic Wise)/3_Slinding_Window/1_Best_time_to_buy_and_sell.cpp
+++ Data Structures and Algorithms with C++/Questions (Topic Wise)/3_Slinding_Window/1_Best_time_to_buy_and_sell.cpp
@@ -1,9 +1,37 @@
 // Q-https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
 
-#include <bits/stdc++.h>
+#include<iostream>
+#include<vector>
+#include<algorithm>
 using namespace std;
 
 class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+
+        int lowest_purchase = prices.at(0);     //the starting price
+        int current_profit = 0;                 //current_profit is being used to check the profit we can have
+        int max_profit = 0 ;                    //this is the max profit we have gotten so far
+
+        for(int i=1; i<prices.size(); i++){
+            
+            if(prices.at(i) < lowest_purchase){
+                lowest_purchase = prices.at(i);
+            }
+
+            current_profit = prices.at(i) - lowest_purchase;
+
+            if(current_profit > max_profit ){
+                max_profit = current_profit;
+            }
+            
+        }
+        return max_profit;
+    }
+};
+
+// this is also O(n) as we are using the algorithms lib for min and max
+class Solution2 {
 public:
     int maxProfit(vector<int>& prices) {
         int minValue = prices[0];
@@ -13,14 +41,13 @@ public:
             minValue = min(minValue, prices[i]);
             maxDiff = max(maxDiff, prices[i] - minValue);
         }
-        
         return maxDiff;
     }
 };
 
 
 //Passing test case but buffer overflow so use the above one !
-class Solution2 {
+class Solution3 {
 public:
     int maxProfit(vector<int>& prices) {
         
--- Data Structures and Algorithms with C++/test.cpp
+++ Data Structures and Algorithms with C++/test.cpp
@@ -14,13 +14,41 @@
 
 */
 #include<iostream>
+#include<string>
 #include<vector>
 #include<unordered_map>
 
 using namespace std;
 
+class Solution {
+public:
+    bool isValid(string s) {
+        
+
+        // make a for loop for the string and in that for loop we can
+            // 1) if i != i+1; return false
+            //i++
+
+        // if the starting bracket is ) ] } then its a flase 
+        if(s.at(0) == ')' || s.at(0) == ']' || s.at(0) == '}') return false;
+
+        for( int i = 0; i<s.size(); i++){ 
+            char current = s[i];
+            char next = s.at(i+1);
+            if( current != next) return false;
+            
+        }
+            
+        return true;
+    }
+};
 
 int main(){
 
+   Solution A;
+   string brackets = "()";
+   
+    cout<<boolalpha;
+   cout<< A.isValid(brackets);
    
 }
