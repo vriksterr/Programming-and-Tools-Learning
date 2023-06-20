27a693d8ea904942abfcba287e9e5f2b95085640
Vineet <vineet@vrikster.com>
2023-06-20 23:37:31 +0530

    file cleaning

--- CPP (C++)/test.cpp
+++ CPP (C++)/test.cpp
@@ -18,18 +18,3 @@
     •
 
 */
-#include <stdio.h>
-#include <sys/types.h>
-#include <unistd.h>
-
-int function(){
-  if(function() == 0){
-    fork();
-    printf("hello world");
-  }
-}
-int main() {
-    fork();
-    printf("Hello, World!\n");
-    return 0;
-}
