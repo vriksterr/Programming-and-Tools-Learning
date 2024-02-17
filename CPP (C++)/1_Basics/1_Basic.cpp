13e545051e67ff5801d7809d30768e7d9c13b938
Vineet <vineet@vrikster.com>
2024-02-17 23:35:19 +0530

    added the library file part to the basics for better understanding

--- CPP (C++)/1_Basics/1_Basic.cpp
+++ CPP (C++)/1_Basics/1_Basic.cpp
@@ -20,6 +20,23 @@ string
 Parameter means for eg
 With int a b c as parameter do addition or substraction on these by being within this a b c cant get d which is not in parameter
 
+     __________________
+    | Librarys of C++ |
+    |_________________|
+    Link for C++ Standaard Library Headers -> https://en.cppreference.com/w/cpp/header
+    
+    The standard library (that is part of the language) has been written exclusively in C++. Also the big mayority of libraries are written in C++.
+    However C++ implements an easy way to incorporate libraries written in other languages. On this way, there are several libraries written in other languages like C or Fortran, for instance.
+    
+    basically a library like <algorithm>, <imgui>, etc which provide abstraction and allow programmers to
+    achieve complex tasks without typing complex code
+
+    To understand it in a deeper way these liberaries provide an abstrations to the programmer for example <imgui>
+    liberary where if your use box::___ you can create a box now in this case you didnt had to write all the code that
+    was required to make that box same way in <algorithm> where we type what calculations to perform without writing 
+    the algorithm ourselfs.
+
+
 */
 #include <iostream>
 using namespace std;
